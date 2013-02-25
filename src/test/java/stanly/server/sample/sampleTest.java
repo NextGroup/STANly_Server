f3bd9f59ab059937d1a28faa36cb8d9f50793e1f
karuana <mse201@gmail.com>
2013-02-26 07:14:53 +0900

    Junit환경 셋팅
    TDD 개발을 위한 Junit 환경 셋팅
    
    Signed-off-by: karuana <mse201@gmail.com>

---
+++ src/test/java/stanly/server/sample/sampleTest.java
@@ -0,0 +1,20 @@
+package stanly.server.sample;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:/applicationContext.xml")
+public class sampleTest {
+
+
+	@Test
+	public void TestTrue()
+	{
+		assertTrue(true);
+	}
+}
