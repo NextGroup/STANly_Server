27c210363b7c4c1adddc5a9eedd348235726274d
YangHyunchul <wolfses@hotmail.com>
2013-03-20 17:22:37 +0900

    Frontend Test페이지 삭제

--- src/main/java/stanly/server/MetricView/Json/Metrics.java
+++ src/main/java/stanly/server/MetricView/Json/Metrics.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 
 public class Metrics {
 	private ArrayList<MetricValue> metricList;
+	public Metrics()
+	{
+		metricList = new ArrayList<MetricValue>();
+	}
 	public boolean 	addValue(String name,float e) {
 		return metricList.add(new MetricValue(name,e));
 	}
